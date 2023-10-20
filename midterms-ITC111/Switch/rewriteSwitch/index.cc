504ce50e725555f70a080d790c9884fa7ed9a24f
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-10-20 19:22:10 +0800

    Convert to switch from ifElse

---
+++ midterms-ITC111/Switch/rewriteSwitch/index.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int facet;
+  float coercion;
+
+  cout << "Enter a numerical value: ";
+  cin >> facet;
+
+  switch(facet){
+    
+    case 1:
+    coercion += 25.0;
+    cout << "The new value of coercion is: " << coercion;
+    break;
+
+    case 2:
+    coercion += 40.0;
+    cout << "The new value of coercion is: " << coercion;
+    break;
+
+    case 3:
+    coercion += 55.0;
+    cout << "The new value of coercion is: " << coercion;
+    break;
+
+    case 4:
+    coercion += 67.0;
+    cout << "The new value of coercion is: " << coercion;
+    break;
+
+    case 5:
+    coercion += 67.0;
+    cout << "The new value of coercion is: " << coercion;
+    break;
+
+    case 6:
+    coercion += 67.0;
+    cout << "The new value of coercion is: " << coercion;
+    break;
+  }
+
+}
